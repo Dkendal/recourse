8ef93f412c6a41e4f18db74ba7f687ff32e915a9
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-10-08 21:09:46 -0700

    Added redux devtools

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,12 @@
-import {createStore, applyMiddleware} from "redux";
+import {compose, createStore, applyMiddleware} from "redux";
 import thunkMiddleware from "redux-thunk";
 import createLogger from "redux-logger";
 import {Provider} from "react-redux";
+
+// Redux dev tools
+import {devTools, persistState} from 'redux-devtools';
+import {DevTools, DebugPanel, LogMonitor} from 'redux-devtools/lib/react';
+
 import React from "react";
 import {List} from "immutable";
 import Recourse from "./containers/Recourse";
@@ -13,9 +18,16 @@ import socket from "./socket";
 // Now that you are connected, you can join channels with a topic:
 const loggerMiddleware = createLogger();
 
-const store = applyMiddleware(
-  thunkMiddleware,
-  loggerMiddleware
+
+const store = compose(
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  ),
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore)(reducer);
 
 const channel = socket.channel("schedules:planner", {});
@@ -23,8 +35,13 @@ const channel = socket.channel("schedules:planner", {});
 store.dispatch(joinChannel(channel));
 
 React.render(
-  <Provider store={store}>
-    {() => <Recourse />}
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      {() => <Recourse />}
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   document.getElementById("app")
 );
