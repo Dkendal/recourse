1af470546b91360f304ab63e1f2073aaddcfd3a6
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-10-10 23:02:09 -0700

    Transfrom scraped sections into records

--- lib/recourse/scraper.ex
+++ lib/recourse/scraper.ex
@@ -1,6 +1,6 @@
 defmodule Recourse.Scraper do
   use HTTPoison.Base
-  use PatternTap
+  use Timex
   alias Recourse.Course
   alias Recourse.Section
   alias Recourse.Repo
@@ -16,8 +16,30 @@ defmodule Recourse.Scraper do
   def process_url(url) do
     "https://www.uvic.ca/BAN2P/"
     <> case url do
-      "courses" -> "bwckctlg.p_display_courses"
+      "courses" ->
+        "bwckctlg.p_display_courses"
+
+      "course?" <> q ->
+        "bwckctlg.p_disp_listcrse?" <> q
     end
+  end
+
+  def course(args) do
+    resp = course_query(args).body
+    |> Floki.find(".datadisplaytable")
+    |> Enum.slice(1..-2) # the first and last rows are garbage
+    |> Floki.find("tr")
+    |> Enum.map(fn tr ->
+      tr
+      |> Floki.find("th, td")
+      |> Enum.map(&Floki.text/1)
+    end)
+    |> Enum.chunk(2)
+    |> Enum.map(fn [th, td] ->
+      Enum.zip(th, td)
+      |> Enum.into(%{})
+      |> transform_section
+    end)
   end
 
   def courses(args) do
@@ -30,6 +52,15 @@ defmodule Recourse.Scraper do
     end
   end
 
+  defp course_query(args) do
+    args
+    |> course_params
+    |> URI.encode_query
+    |> case do
+      q -> get!("course?" <> q)
+    end
+  end
+
   defp courses_query(args) do
     args
     |> courses_params
@@ -37,6 +68,15 @@ defmodule Recourse.Scraper do
     |> case do
       q -> post!("courses", q)
     end
+  end
+
+  defp course_params([year, semester, subject, number]) do
+    %{
+      crse_in: number,
+      schd_in: "",
+      subj_in: subject,
+      term_in: term(year, semester)
+    }
   end
 
   defp courses_params([year, semester, subjects, course_start, course_end]) do
@@ -70,6 +110,68 @@ defmodule Recourse.Scraper do
     end
   end
   def parse(_, _), do: :error
+
+  @dateformat "{Mshort} {D}, {YYYY}"
+  def transform_section(%{"Date Range" => date_range} = map) do
+    [date_start, date_end] =
+      date_range
+      |> String.split(" - ")
+      |> Enum.map fn date ->
+        date
+        |> DateFormat.parse(@dateformat)
+        |> case do
+          {:ok, date} -> date
+        end
+      end
+
+    map
+    |> Map.put(:date_start, date_start)
+    |> Map.put(:date_end, date_end)
+    |> Map.delete("Date Range")
+    |> transform_section
+  end
+
+  def transform_section(%{"Days" => days} = map) do
+    days = String.codepoints days
+
+    map
+    |> Map.put(:days, days)
+    |> Map.delete("Days")
+    |> transform_section
+  end
+
+  @timeformat "{h12}:{m} {am}"
+  def transform_section(%{"Time" => time} = map) do
+    [time_start, time_end] =
+      time
+      |> String.split(" - ")
+      |> Enum.map fn time ->
+        time
+        |> DateFormat.parse(@timeformat)
+        |> case do
+          {:ok, time} -> time
+        end
+      end
+
+    map
+    |> Map.put(:time_start, time_start)
+    |> Map.put(:time_end, time_end)
+    |> Map.delete("Time")
+    |> transform_section
+  end
+
+  def transform_section(map) do
+    map
+    |> Map.put(:schedule_type, map["Schedule Type"])
+    |> Map.put(:location, map["Where"])
+    |> Map.delete("Instructors")
+    |> Map.delete("Schedule Type")
+    |> Map.delete("Type")
+    |> Map.delete("Where")
+    |> case do
+      map -> Map.merge %Recourse.Section{}, map
+    end
+  end
 
   defp term(year, semester) do
     Integer.to_string(year) <> @semesters[semester]
--- test/recourse/scraper_test.exs
+++ test/recourse/scraper_test.exs
@@ -1,23 +1,37 @@
 defmodule Recourse.ScraperTest do
   use ExUnit.Case
   alias Recourse.Term
+  import Ecto.Query
 
-  test "returns a list of courses" do
+  setup do
     Recourse.Scraper.start
+    :ok
+  end
 
+  test "fetching a list of courses" do
     actual =
       Recourse.Scraper.courses([2015, :spring, ["CSC"], "100", "200"])
 
+    assert is_list(actual)
     assert length(actual) > 0
     assert Enum.all?(actual, &is_course?/1)
   end
 
+  @tag timeout: :infinity
+  test "fetching sections for a course" do
+    actual =
+      Recourse.Scraper.course([2015, :spring, "CSC", "110"])
+
+    assert is_list(actual)
+    assert length(actual) > 0
+    assert Enum.all?(actual, &is_section?/1)
+  end
+
   test "parsing a course" do
-    expected =
-      %Recourse.Course{
-        subject: "CSC",
-        number: "100",
-        title: "ELEMENTARY COMPUTING" }
+    expected = %Recourse.Course{
+      subject: "CSC",
+      number: "100",
+      title: "ELEMENTARY COMPUTING" }
 
     actual =
       Recourse.Scraper.parse("CSC 100 - ELEMENTARY COMPUTING", :course)
@@ -25,6 +39,40 @@ defmodule Recourse.ScraperTest do
     assert expected == actual
   end
 
+  test "transforming a section" do
+    input = %{
+      "Date Range" => "Jan 05, 2015 - Apr 02, 2015",
+      "Days" => "MR",
+      "Instructors" => "Celina Gay  Berg (P)",
+      "Schedule Type" => "Lecture",
+      "Time" => "10:00 am - 11:20 am",
+      "Type" => "Every Week",
+      "Where" => "MacLaurin Building A144"}
+
+    actual =
+      Recourse.Scraper.transform_section(input)
+
+    # expected = %Recourse.Section{
+    #   registration_code:
+    #   schedule_type:
+    #   time_start:
+    #   time_end:
+    #   days:
+    #   location:
+    #   date_start: %Ecto.Date{year: 2015, month: 1, }
+    #   date_end:
+    #   registration_start:
+    #   registration_end:
+    #   campus:
+    #   credits:
+    #   instructional_method:
+    # }
+
+  end
+
   defp is_course?(%Recourse.Course{}), do: true
   defp is_course?(_), do: false
+
+  defp is_section?(%Recourse.Section{}), do: true
+  defp is_section?(_), do: false
 end
