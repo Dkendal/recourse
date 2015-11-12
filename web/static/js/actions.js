215de6f47874c2a786a6fccd47e2229e519100b1
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-11 22:28:13 -0800

    Reorganized actions
    Tried to group const declerations with associated actions.

--- web/static/js/actions.js
+++ web/static/js/actions.js
@@ -1,33 +1,29 @@
 import {createAction} from "redux-actions";
 import {selectedCourses} from "./selectors";
 
-export const SELECT_COURSE = "SELECT_COURSE";
 export const DESELECT_COURSE = "DESELECT_COURSE";
-export const SET_COURSES = "SET_COURSES";
-export const SET_SECTIONS = "SET_SECTIONS";
-export const JOINING_CHANNEL = "JOINING_CHANNEL";
-export const JOINED_CHANNEL = "JOINED_CHANNEL";
-
-const setCourses = createAction(SET_COURSES);
-const setSections = createAction(SET_SECTIONS);
-
-export const selectCourse = createAction(SELECT_COURSE, ({id}) => id);
 export const deselectCourse = createAction(DESELECT_COURSE, ({id}) => id);
 
-export const joiningChannel = createAction(JOINING_CHANNEL);
+export const JOINED_CHANNEL = "JOINED_CHANNEL";
 export const joinedChannel = createAction(JOINED_CHANNEL);
 
-function getCourseIds(getState) {
-  return selectedCourses(getState());
-}
+export const JOINING_CHANNEL = "JOINING_CHANNEL";
+export const joiningChannel = createAction(JOINING_CHANNEL);
 
+export const SELECT_COURSE = "SELECT_COURSE";
+export const selectCourse = createAction(SELECT_COURSE, ({id}) => id);
+
+export const SET_SECTIONS = "SET_SECTIONS";
 function updateSchedule(startAction) {
   return channel => course => (dispatch, getState) => {
+    const setSections = createAction(SET_SECTIONS);
+
     dispatch(startAction(course));
 
-    const courseIds = getCourseIds(getState);
+    const courseIds = selectedCourses(getState());
 
-    channel.push("make_schedule", courseIds)
+    channel
+    .push("make_schedule", courseIds)
     .receive(
       "ok",
       ({payload}) => {
@@ -53,9 +49,13 @@ export function toggleCourseSelection(channel, selectedCourses, course) {
   return addToSchedule(channel)(course);
 }
 
+export const SET_COURSES = "SET_COURSES";
 export function searchCourses(channel) {
   return (dispatch) => {
-    channel.push("courses:search")
+    const setCourses = createAction(SET_COURSES);
+
+    channel
+    .push("courses:search")
     .receive(
       "ok",
       ({payload}) => {
@@ -76,7 +76,8 @@ export function joinChannel(channel) {
   return dispatch => {
     dispatch(joiningChannel(channel));
 
-    channel.join()
+    channel
+    .join()
     .receive(
       "ok",
       resp => {
