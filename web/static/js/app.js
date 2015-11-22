028c77c26dc6dee1689b60965fca2f848bdecdd1
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-22 14:10:54 -0800

    upgraded react to v14.0

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,13 +2,8 @@ import {compose, createStore, applyMiddleware} from "redux";
 import thunkMiddleware from "redux-thunk";
 import createLogger from "redux-logger";
 import {Provider} from "react-redux";
-
-// Redux dev tools
-import {devTools, persistState} from 'redux-devtools';
-import {DevTools, DebugPanel, LogMonitor} from 'redux-devtools/lib/react';
-
 import React from "react";
-import {List} from "immutable";
+import ReactDOM from "react-dom";
 import Recourse from "./containers/Recourse";
 import reducer from "./reducers";
 import {joinChannel} from "./actions";
@@ -18,30 +13,22 @@ import socket from "./socket";
 // Now that you are connected, you can join channels with a topic:
 const loggerMiddleware = createLogger();
 
-
 const store = compose(
   applyMiddleware(
     thunkMiddleware,
     loggerMiddleware
-  ),
-  // Provides support for DevTools:
-  devTools(),
-  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  )
 )(createStore)(reducer);
 
 const channel = socket.channel("schedules:planner", {});
 
 store.dispatch(joinChannel(channel));
 
-React.render(
+ReactDOM.render(
   <div>
     <Provider store={store}>
-      {() => <Recourse />}
+      <Recourse />
     </Provider>
-    <DebugPanel top right bottom>
-      <DevTools store={store} monitor={LogMonitor} visibleOnLoad={false}/>
-    </DebugPanel>
   </div>,
   document.getElementById("app")
 );
