2dee1c3fc5fd650996926023d7030dc937ce4309
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-22 17:20:43 -0800

    Formatting

--- web/static/js/components/ScheduleSection.jsx
+++ web/static/js/components/ScheduleSection.jsx
@@ -1,11 +1,10 @@
 import React, {Component, PropTypes} from "react";
-import {SECTION} from "../types";
 
 function hash(str) {
   return(
-    str
-      .split("")
-      .reduce(
+    str.
+      split("").
+      reduce(
         (a, b) => {
           a = ((a << 5) -a) + b.charCodeAt(0);
           return a & a;
@@ -17,17 +16,17 @@ function hash(str) {
 
 function color({subject, number}) {
   const str = `${subject}${number}`;
-  const hex = hash(str)
-    .toString(16)
-    .slice(-6);
+  const hex = hash(str).
+    toString(16).
+    slice(-6);
 
   return `#${hex}`;
 }
 
 function toPercent(t, startHr, endHr) {
-  const [hr, min] = t
-    .split(":")
-    .map(x => Number.parseInt(x, 10));
+  const [hr, min] = t.
+    split(":").
+    map(x => Number.parseInt(x, 10));
 
   const scale = endHr - startHr;
 
@@ -55,8 +54,8 @@ export default class ScheduleSection extends Component {
       (day) => [
         "schedule-section",
         "schedule-day-" + day
-      ]
-      .join(" ");
+      ].
+      join(" ");
 
     return (
       <div className="schedule-sections">
