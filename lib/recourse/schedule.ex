aaa580acabf7d4dfa5d77a578b2b9fae2b2552ef
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-25 22:58:59 -0800

    Refactored schedule builder

--- lib/recourse/schedule.ex
+++ lib/recourse/schedule.ex
@@ -7,27 +7,12 @@ defmodule Recourse.Schedule do
   def build course_ids do
     {:ok, pid} = Aruspex.start_link
 
-    courses = (from s in Section,
-    select: s,
-    join: c in assoc(s, :course),
-    where: c.id in ^course_ids,
-    preload: [:course],
-    order_by: c.id)
-    |> Repo.all # [Section]
-    |> Enum.group_by(& {&1.course_id, &1.schedule_type}) # %{ int: [Section] }
-    |> Enum.map(fn {course, sections} ->
-      pid |> Aruspex.variable(course, sections)
-      course
-    end)
+    course_ids
+    |> get_sections
+    |> init_variables(pid)
+    |> init_constraints(pid)
 
-    for x <- courses, y <- courses, x > y do
-      pid |> Aruspex.post constraint(
-      variables: [x, y],
-      function: &no_conflict/1)
-    end
-
-    Aruspex.find_solution(pid)
-    |> Dict.values
+    Dict.values Aruspex.find_solution(pid)
   end
 
   def no_conflict([a, b]) do
@@ -52,5 +37,32 @@ defmodule Recourse.Schedule do
     a = Enum.into a.days, HashSet.new
     b = Enum.into b.days, HashSet.new
     Set.disjoint? a, b
+  end
+
+
+  defp init_variables(sections, pid) do
+    sections
+    |> Enum.group_by(& {&1.course_id, &1.schedule_type}) # %{ int: [Section] }
+    |> Enum.map fn {grouping, sections} ->
+      Aruspex.variable(pid, grouping, sections)
+      grouping
+    end
+  end
+
+  defp init_constraints(variables, pid) do
+    for x <- variables, y <- variables, x > y do
+      Aruspex.post pid, constraint(
+        variables: [x, y],
+        function: &no_conflict/1)
+    end
+  end
+
+  defp get_sections course_ids do
+    Repo.all from s in Section,
+      select: s,
+      join: c in assoc(s, :course),
+      where: c.id in ^course_ids,
+      preload: [:course],
+      order_by: c.id
   end
 end
