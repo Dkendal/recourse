ae429f2825d098265389a749b79f0179905b6bc8
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-29 20:10:58 -0800

    Broke more functions out into actions/courses

--- web/static/js/actions.js
+++ web/static/js/actions.js
@@ -1,12 +1,6 @@
 import {createAction} from "redux-actions";
 import channelActions from "./actions/channel";
-import * as s from "./selectors";
-
-const selectCourse = createAction("SELECT_COURSE", ({id}) => id);
-const deselectCourse = createAction("DESELECT_COURSE", ({id}) => id);
-
-const addToSchedule = updateSchedule(selectCourse);
-const removeFromSchedule = updateSchedule(deselectCourse);
+import courseActions from "./actions/courses";
 
 const filterCourses = createAction("FILTER_COURSES");
 
@@ -23,28 +17,10 @@ function changeTerm(term) {
   };
 }
 
-function updateSchedule(startAction) {
-  return course => (dispatch) => {
-    dispatch(startAction(course));
-    dispatch(channelActions.refreshSchedule());
-  };
-}
-
-function toggleCourseSelection(course) {
-  return (dispatch, getState) => {
-    const ids = s.worklistIds(getState());
-
-    if(ids.includes(course.id)) {
-      return dispatch(removeFromSchedule(course));
-    }
-    return dispatch(addToSchedule(course));
-  };
-}
-
 export default {
   ...channelActions,
+  ...courseActions,
   changeTerm,
   filterCourses,
-  setScheduleSettings,
-  toggleCourseSelection
+  setScheduleSettings
 };
