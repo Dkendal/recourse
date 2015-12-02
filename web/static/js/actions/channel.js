89e3481e6a39c9f1af0d92c5b93ba34e89e2f2f0
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-12-01 19:56:14 -0800

    Refactor channel action
    make it so that joinChannel just returns a channel

--- web/static/js/actions/channel.js
+++ web/static/js/actions/channel.js
@@ -1,5 +1,4 @@
 import {createAction} from "redux-actions";
-import action from "../actions";
 import * as s from "../selectors";
 import {compose} from "underscore";
 
@@ -8,65 +7,59 @@ const joiningChannel = createAction("JOINING_CHANNEL");
 const setSections = createAction("SET_SECTIONS");
 const setTerms = createAction("SET_TERMS");
 
+const getPayload = x => x.payload;
+
+function asPromise(push, callback) {
+  const promise = (resolve, reject) => {
+    const onOk = (response) => {
+      callback(response);
+      return resolve();
+    };
+
+    const onError = () => {
+      return reject();
+    };
+
+    push.
+      receive("ok", onOk).
+      receive("error", onError);
+  };
+
+  return new Promise(promise);
+}
+
 function refreshSchedule() {
   return (dispatch, getState) => {
+    const channel = s.channel(getState());
     const scheduleSettings = s.scheduleSettings(getState());
     const ids = s.worklistIds(getState());
+    const onSuccess = compose(dispatch, setSections, getPayload);
 
-    s.channel(getState()).
-      push(
-        "make_schedule",
-        { course_ids: ids, settings: scheduleSettings }
-      ).
-      receive(
-        "ok",
-        ({payload}) => dispatch(setSections(payload))
-      );
+    const params = {
+      course_ids: ids,
+      settings: scheduleSettings
+    };
+
+    const push = channel.
+      push("make_schedule", params);
+
+    return asPromise(push, onSuccess);
   };
 }
 
 function getTerms() {
   return (dispatch, getState) => {
     const channel = s.channel(getState());
-
-    const promise = (resolve, reject) => {
-      channel.
-        push("terms:search").
-        receive(
-          "ok",
-          ({payload}) => {
-            dispatch(setTerms(payload));
-            resolve();
-          }
-        ).
-        receive(
-          "error",
-          () => reject()
-        );
-    };
-
-    return new Promise(promise);
+    const onSuccess = compose(dispatch, setTerms, getPayload);
+    return asPromise(channel.push("terms:search"), onSuccess);
   };
 }
 
 function joinChannel(channel) {
-  return dispatch => {
-    const onOk = () => {
-      dispatch(joinedChannel());
-
-      // get terms, then refresh schedule.
-      dispatch(getTerms()).
-        then(compose(dispatch, refreshSchedule));
-    };
-
+  return (dispatch) => {
+    const onSuccess = compose(dispatch, joinedChannel);
     dispatch(joiningChannel(channel));
-
-    channel.
-      join().
-      receive(
-        "ok",
-        onOk
-      );
+    return asPromise(channel.join(), onSuccess);
   };
 }
 
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -7,10 +7,11 @@ import createEngine from "redux-storage/engines/localStorage";
 import {decorators} from "redux-storage";
 import React from "react";
 import ReactDOM from "react-dom";
+import _ from "underscore";
 import Recourse from "./containers/Recourse";
 import reducer from "./reducers";
 import socket from "./socket";
-import {joinChannel} from "./actions/channel";
+import {joinChannel, getTerms, refreshSchedule} from "./actions/channel";
 
 const channel = socket.channel("schedules:planner", {});
 let engine = createEngine("Recourse");
@@ -34,10 +35,15 @@ const store = compose(
   )
 )(createStore)(storage.reducer(reducer));
 
+const dispatch = store.dispatch;
+
 const load = storage.createLoader(engine);
+
 load(store);
 
-store.dispatch(joinChannel(channel));
+dispatch(joinChannel(channel)).
+  then(_.compose(dispatch, getTerms)).
+  then(_.compose(dispatch, refreshSchedule));
 
 ReactDOM.render(
   <div>
