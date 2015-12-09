cdedfa6e8f6adc499eedd7f284f1903dbc11071b
Dylan Kendal <dylan@stembolt.com>
2015-12-08 18:57:23 -0800

    rewrote tests for schedule

--- test/recourse/schedule_test.exs
+++ test/recourse/schedule_test.exs
@@ -1,171 +1,40 @@
 defmodule Recourse.ScheduleTest do
-  use ExUnit.Case
-  alias Recourse.Repo
-  alias Recourse.Term
-  alias Recourse.Course
-  alias Recourse.Section
   alias Recourse.Schedule
-  alias Ecto.Time
-  import Enum, only: [any?: 2, find: 2, count: 1, member?: 2]
-  import Repo, only: [insert: 1]
+  use Recourse.Case
 
-  setup do
-    {:ok, term} = insert %Term{year: 2015, semester: :summer}
+  test "[build/1]" do
+    term = create(:term, year: 2016, semester: :summer)
+    csc = create(:course, subject: "CSC", number: "100", term: term)
+    engl = create(:course, subject: "ENGL", number: "100", term: term)
 
-    {:ok, csc} = insert %Course{
-      subject: "CSC", number: "100", term_id: term.id
-    }
+    lecture = build(:section,
+      time_start: "12:00:00",
+      time_end: "13:50:00",
+      course: csc
+    )
+    |> as_lecture
+    |> cast
+    |> create
 
-    {:ok, engl} = insert %Course{
-      subject: "ENGL", number: "100", term_id: term.id
-    }
+    tutorial = build(:section,
+      time_start: cast_time("14:00:00"),
+      time_end: cast_time("15:50:00"),
+      course: csc
+    )
+    |> as_tutorial
+    |> cast
+    |> create
 
-    {:ok, math} = insert %Course{
-      subject: "MATH", number: "100", term_id: term.id
-    }
-
-    {:ok, csc_lecture} = insert %Section{
-      days: ~W(M W R),
-      schedule_type: "Lecture",
-      time_end: %Time{hour: 16, min: 20, sec: 0},
-      time_start: %Time{hour: 15, min: 30, sec: 0},
-      course_id: csc.id
-    }
-
-    {:ok, engl_lecture} = insert %Section{
-      days: ~W(M W R),
-      schedule_type: "Lecture",
-      time_end: %Time{hour: 16, min: 20, sec: 0},
-      time_start: %Time{hour: 15, min: 30, sec: 0},
-      course_id: engl.id
-    }
-
-    {:ok, csc_tutorial} = insert %Section{
-      days: ~W(T),
-      schedule_type: "Tutorial",
-      time_end: %Time{hour: 16, min: 20, sec: 0},
-      time_start: %Time{hour: 15, min: 30, sec: 0},
-      course_id: csc.id
-    }
-
-    {:ok, earlier_math_lecture} = insert %Section{
-      days: ~W(M W R),
-      schedule_type: "Lecture",
-      time_end: %Time{hour: 16, min: 20, sec: 0},
-      time_start: %Time{hour: 15, min: 30, sec: 0},
-      course_id: math.id
-    }
-
-    {:ok, late_math_lecture} = insert %Section{
-      days: ~W(M W R),
-      schedule_type: "Lecture",
-      time_start: %Time{hour: 19, min: 30, sec: 0},
-      time_end: %Time{hour: 20, min: 20, sec: 0},
-      course_id: math.id
-    }
-
-    {:ok, _} = insert %Section{
-      days: ~W(F),
-      schedule_type: "Lab",
-      time_end: %Time{hour: 16, min: 20, sec: 0},
-      time_start: %Time{hour: 15, min: 30, sec: 0},
-      course_id: math.id
-    }
-
-    { :ok,
-      math: math,
-      csc: csc,
-      engl: engl,
-      course_ids: [math.id, csc.id],
-      late_math_lecture: late_math_lecture,
-      earlier_math_lecture: earlier_math_lecture,
-      engl_lecture: engl_lecture,
-      csc_lecture: csc_lecture,
-      csc_tutorial: csc_tutorial
-    }
-  end
-
-  test "[build/1] groups conflicts togethor", context do
     sections = Schedule.build(%{
-      "course_ids" => [context.csc.id, context.engl.id],
+      "course_ids" => [csc.id, engl.id],
       "settings" => %{
         "startTime" => "00:00:00",
         "endTime" => "00:00:00"
       }
     })
 
-    assert count(sections) == 4
-
-    assert any? sections, fn
-      [s] ->
-        s.id == context.csc_tutorial.id
-      _ ->
-        false
-    end
-
-    assert any? sections, fn
-      [x, y] ->
-        (
-          x.id == context.csc_lecture.id &&
-          y.id == context.engl_lecture.id
-        )
-        or
-        (
-          y.id == context.csc_lecture.id &&
-          x.id == context.engl_lecture.id
-        )
-      _ ->
-        false
-    end
-  end
-
-  test "[build/1]", %{course_ids: course_ids, late_math_lecture: math_lecture} do
-    sections = Schedule.build(%{
-      "course_ids" => course_ids,
-      "settings" => %{
-        "startTime" => "00:00:00",
-        "endTime" => "00:00:00"
-      }
-    })
-
-    assert count(sections) == 8
-
-    assert any? sections, fn [s] ->
-      s.id == math_lecture.id
-    end
-  end
-
-  test "[build/1] can support time preferences", context do
-    actual = Schedule.build(%{
-      "course_ids" => [context.math.id],
-      "settings" => %{
-        "startTime" => "00:00:00",
-        "endTime" => "19:00:00"
-      }
-    })
-
-    assert any? actual, fn [s] ->
-      s.id == context.earlier_math_lecture.id
-    end
-
-    refute any? actual, fn [s] ->
-      s.id == context.late_math_lecture.id
-    end
-
-    actual = Schedule.build(%{
-      "course_ids" => [context.math.id],
-      "settings" => %{
-        "startTime" => "19:00:00",
-        "endTime" => "00:00:00"
-      }
-    })
-
-    refute any? actual, fn [s] ->
-      s.id == context.earlier_math_lecture.id
-    end
-
-    assert any? actual, fn [s] ->
-      s.id == context.late_math_lecture.id
-    end
+    assert length(sections) == 2
+    assert Enum.any? sections, & &1.id == lecture.id
+    assert Enum.any? sections, & &1.id == tutorial.id
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -24,19 +24,29 @@ defmodule Recourse.Factory do
       campus: "main campus",
       course: build(:course),
       credits: 3.0,
-      date_end: "2015-04-01" |> cast_date,
-      date_start: "2015-01-01" |> cast_date,
-      days: ~W(MWF),
+      date_end: "2015-04-01",
+      date_start: "2015-01-01",
+      days: ~W(M W F),
       instructional_method: "face-to-face",
       location: "some building",
       name: sequence(:section_name, &"#{&1}"),
       registration_code: sequence(:crn, &"C#{&1}"),
-      registration_end: "2015-01-15" |> cast_date,
-      registration_start: "2014-09-30" |> cast_date,
+      registration_end: "2015-01-15",
+      registration_start: "2014-09-30",
       schedule_type: "Lecture",
-      time_end: "13:20:00" |> cast_time,
-      time_start: "12:00:00" |> cast_time,
+      time_end: "13:20:00",
     }
+  end
+
+  def cast(p) do
+    params = Map.drop p, [:__struct__,]
+
+    updated = p.__struct__.changeset(
+      struct(p.__struct__),
+      params
+    ).changes
+
+    Map.merge p, updated
   end
 
   def as_lecture(section) do
@@ -51,13 +61,13 @@ defmodule Recourse.Factory do
     %{section | schedule_type: "Tutorial"}
   end
 
-  defp cast_date({:ok, d}), do: d
-  defp cast_date d do
+  def cast_date({:ok, d}), do: d
+  def cast_date d do
     d |> Date.cast |> cast_date
   end
 
-  defp cast_time({:ok, t}), do: t
-  defp cast_time t do
+  def cast_time({:ok, t}), do: t
+  def cast_time t do
     t |> Time.cast |> cast_time
   end
 end
