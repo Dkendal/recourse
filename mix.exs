c0d2b2cbcc766676e35261df5ab190e1280721cb
Dylan Kendal <dylan@stembolt.com>
2015-12-09 21:42:52 -0800

    Bump phoenix patch version
    1.0.1 was not compatible with elixir 1.1 as it assumed that apps were
    all started automatically.

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Recourse.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [
-      {:phoenix, "~> 1.0.1"},
+      {:phoenix, "~> 1.0"},
       {:phoenix_html, "~> 2.1"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:aruspex, path: "~/src/aruspex"},
