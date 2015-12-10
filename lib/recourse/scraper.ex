ba1715be839a2229faca80ab3b9a620bfd473052
Dylan Kendal <dylan@stembolt.com>
2015-12-09 22:28:58 -0800

    Added logging output to the scraper.
    Logs all request and responses.

--- lib/recourse/scraper.ex
+++ lib/recourse/scraper.ex
@@ -1,9 +1,9 @@
 defmodule Recourse.Scraper do
   use HTTPoison.Base
+  require Logger
 
-  def process_url(url) do
-    "https://www.uvic.ca/BAN2P/"
-    <> case url do
+  def process_url(path) do
+    path = case path do
       "courses?" <> q ->
         "bwckctlg.p_display_courses?" <> q
 
@@ -13,5 +13,22 @@ defmodule Recourse.Scraper do
       "seats" <> q ->
         "bwckschd.p_disp_detail_sched" <> q
     end
+
+    "https://www.uvic.ca/BAN2P/" <> path
+  end
+
+  def get!(url) do
+    Logger.info "#{inspect self} GET #{inspect url}"
+    super(url)
+  end
+
+  def post!(url, opts) do
+    Logger.info "#{inspect self} POST #{inspect url}\n\t#{inspect opts}"
+    super(url, opts)
+  end
+
+  defp process_status_code(status_code) do
+    Logger.info "#{inspect self} Response #{status_code}"
+    status_code
   end
 end
