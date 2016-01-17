ff1b9bb5cbdaecf693ad86e223fbf28fee6cba25
Dylan Kendal <dylan@stembolt.com>
2016-01-16 18:38:46 -0800

    use describe/it for test

--- test/recourse/schedule_test.exs
+++ test/recourse/schedule_test.exs
@@ -7,35 +7,37 @@ defmodule Recourse.ScheduleTest do
     :ok
   end
 
-  test "[build/1]" do
-    term = create(:term)
+  describe "build/1" do
+    it "returns a schedule for the selected courses" do
+      term = create(:term)
 
-    csc = create(:course, term: term)
-    engl = create(:course, term: term)
+      csc = create(:course, term: term)
+      engl = create(:course, term: term)
 
-    csc_lecture = build(:section, course: csc)
-    |> as_lecture
-    |> create
+      csc_lecture = build(:section, course: csc)
+                    |> as_lecture
+                    |> create
 
-    csc_tutorial = build(:section, course: csc)
-    |> as_tutorial
-    |> create
+      csc_tutorial = build(:section, course: csc)
+                      |> as_tutorial
+                      |> create
 
-    engl_lecture = build(:section, course: engl)
-    |> as_tutorial
-    |> create
+      engl_lecture = build(:section, course: engl)
+                      |> as_tutorial
+                      |> create
 
-    sections = Schedule.build(%{
-      "course_ids" => [csc.id, engl.id],
-      "settings" => %{
-        "startTime" => "00:00:00",
-        "endTime" => "00:00:00"
-      }
-    })
+      sections = Schedule.build(%{
+        "course_ids" => [csc.id, engl.id],
+        "settings" => %{
+          "startTime" => "00:00:00",
+          "endTime" => "00:00:00"
+        }
+      })
 
-    assert length(sections) == 3
-    assert Enum.any? sections, & &1.id == csc_lecture.id
-    assert Enum.any? sections, & &1.id == csc_tutorial.id
-    assert Enum.any? sections, & &1.id == engl_lecture.id
+      assert length(sections) == 3
+      assert Enum.any? sections, & &1.id == csc_lecture.id
+      assert Enum.any? sections, & &1.id == csc_tutorial.id
+      assert Enum.any? sections, & &1.id == engl_lecture.id
+    end
   end
 end
