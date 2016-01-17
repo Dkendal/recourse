5062ad54186d2ce913a9527f24b1735cb20cb5cf
Dylan Kendal <dylan@stembolt.com>
2016-01-16 20:31:44 -0800

    bump elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Recourse.Mixfile do
   def project do
     [app: :recourse,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
