4633aaa3df7f6028f02c5051b41d947972d1994d
Dylan Kendal <dylan@stembolt.com>
2016-01-25 12:36:44 -0800

    Write Course component as functional components

--- web/static/js/components/Course.jsx
+++ web/static/js/components/Course.jsx
@@ -1,36 +1,28 @@
-import React, {Component} from "react";
-import {COURSE} from "../types";
+import React from "react";
+import {COURSE} from "types";
 import "css/components/Course";
 
-export default class Course extends Component {
-  render() {
-    const {
-      number,
-      subject,
-      title,
-      selected,
-      onClick
-    } = this.props;
+const text = (selected) => selected ? "-" : "+";
 
-    const text = selected ? "-" : "+";
-    return(
-      <tr className="Course">
-        <td>{number}</td>
-        <td>{subject}</td>
-        <td>{title}</td>
-        <td>
-          <button
-            className="Course-toggle"
-            onClick={onClick}
-          >
-            {text}
-          </button>
-        </td>
-      </tr>
-    );
-  }
-}
+const Course = ({number, subject, title, selected, onClick}) => (
+  <tr className="Course">
+    <td>{number}</td>
+    <td>{subject}</td>
+    <td>{title}</td>
+    <td>
+      <button
+        className="Course-toggle"
+        onClick={onClick}
+        >
+        {text(selected)}
+      </button>
+    </td>
+  </tr>
+);
+
 
 Course.displayName = "Course";
 
 Course.propTypes = COURSE;
+
+export default Course;
