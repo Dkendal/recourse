75db60e93d09cd5a06550476e91a679139dd117c
Dylan Kendal <dylan@stembolt.com>
2016-01-30 10:20:26 -0800

    Include a $type value for ecto values in serialization

--- lib/ecto.ex
+++ lib/ecto.ex
@@ -1,13 +1,13 @@
 defimpl Poison.Encoder, for: Ecto.Date do
   def encode(%Ecto.Date{year: y, month: m, day: d}, _options) do
-    %{years: y, months: m, days: d}
+    %{years: y, months: m, days: d, "$type": "date"}
     |> Poison.Encoder.encode([])
   end
 end
 
 defimpl Poison.Encoder, for: Ecto.Time do
   def encode(%Ecto.Time{hour: h, min: m, sec: s}, _options) do
-    %{hours: h, minutes: m, seconds: s}
+    %{hours: h, minutes: m, seconds: s, "$type": "time"}
     |> Poison.Encoder.encode([])
   end
 end
