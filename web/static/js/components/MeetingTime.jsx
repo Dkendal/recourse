f4f2eab4b2dc365643ecdb329dc300b1958346b7
Dylan Kendal <dylan@stembolt.com>
2016-01-30 12:57:23 -0800

    Properly display conflicts again

--- web/static/js/components/MeetingTime.jsx
+++ web/static/js/components/MeetingTime.jsx
@@ -20,21 +20,22 @@ const TextArea = ({children, ...rest}) => (
   </switch>
 );
 
-const MeetingTime = ({meetingTime, section, colorScale, yScale, xScale}) => {
+const MeetingTime = ({meetingTime, section, colorScale, yScale, xScale, conflicts, idx}) => {
   const course = section.course;
   const start = moment(meetingTime.time_start);
   const end = moment(meetingTime.time_end);
   const y1 = yScale(start);
   const y2 = yScale(end);
   const [x1, x2] = xScale.invertExtent(day(meetingTime));
+  const width = (x2 - x1) / conflicts;
 
   // const width = (right - left) / conflicts;
 
   const options =  {
     fill:    colorScale(section),
-    x:       `${x1}%`,
+    x:       `${x1 + idx * width}%`,
     y:       `${y1}%`,
-    width:   `${x2 - x1}%`,
+    width:   `${width}%`,
     height:  `${y2 - y1}%`,
     rx: '5px',
     ry: '5px',
