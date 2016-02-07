15d8f70e0f4b82c8481eefd7e7f11c3805505193
Dylan Kendal <dylan@stembolt.com>
2016-02-06 22:40:21 -0800

    Add assertion for 2 course query

--- test/recourse/scraper/section/request_test.exs
+++ test/recourse/scraper/section/request_test.exs
@@ -35,10 +35,17 @@ defmodule Recourse.Scraper.Section.RequestTest do
 
     context "with 2 courses in different subjects" do
       it "would issue 2 requests" do
-        csc = build(:course, subject: "CSC")
-        seng = build(:course, subject: "SENG")
+        csc = build(:course, subject: "CSC", number: 100)
+        seng = build(:course, subject: "SENG", number: 100)
 
         assert 2 == Request.query_plan([csc, seng]) |> length
+
+        assert [
+          {[^csc],
+            "sections?crse_in=100&schd_in=&subj_in=CSC&term_in=201501"},
+          {[^seng],
+            "sections?crse_in=100&schd_in=&subj_in=SENG&term_in=201501"},
+        ] = Request.query_plan([csc, seng])
       end
     end
   end
