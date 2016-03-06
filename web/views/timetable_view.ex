6bc0ad059981b0dcd2be4acffa756a348dd6144c
Dylan Kendal <dylan@stembolt.com>
2016-03-06 16:24:26 +0800

    Basic section view

--- web/views/timetable_view.ex
+++ web/views/timetable_view.ex
@@ -1,8 +1,25 @@
+defmodule Recourse.SectionView do
+  use JSONAPI.PhoenixView
+
+  def fields(), do: [
+    :crn,
+    :name,
+    :schedule_type,
+    :seats,
+    :waitlist,
+  ]
+
+  def type(), do: "section"
+  def relationships(), do: [
+  ]
+end
+
 defmodule Recourse.TimetableView do
   use JSONAPI.PhoenixView
 
   def fields(), do: []
   def type(), do: "timetable"
   def relationships(), do: [
+    sections: {Recourse.SectionView, :include},
   ]
 end
