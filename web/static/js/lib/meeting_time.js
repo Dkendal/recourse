32ff4de8e7bd6dea5c4047d5e2728ddead5a107a
Dylan Kendal <dylan@stembolt.com>
2016-03-09 07:59:47 +0800

    Set color value on meeting time

--- web/static/js/lib/meeting_time.js
+++ web/static/js/lib/meeting_time.js
@@ -8,11 +8,31 @@ function toDate(string) {
   return moment(string, "YYYY-MM-DD").toDate();
 }
 
+function toColor(mt) {
+  function toNum(str) {
+    let value = 0;
+
+    for (let idx = 0; idx < str.length; idx++) {
+      value += str.charCodeAt(idx);
+    }
+
+    return value
+  }
+
+  const course = mt.section.course;
+  const {subject, number} = course
+
+  const hue = (toNum(subject) ** 5 + toNum(number)) * 16 % 255;
+
+  return `hsl(${hue},${80}%,${70}%)`;
+}
+
 export function cast(mt) {
   const start_time = toTime(mt.start_time);
   const end_time = toTime(mt.end_time);
   const date_end = toDate(mt.date_end)
   const date_start = toDate(mt.date_start)
+  const color = toColor(mt)
 
   return {
     ...mt,
