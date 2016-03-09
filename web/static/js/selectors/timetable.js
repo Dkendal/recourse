2bb055b825e100c196a62c1fca5b72625926a1bc
Dylan Kendal <dylan@stembolt.com>
2016-03-09 08:00:06 +0800

    selector for time table times

--- web/static/js/selectors/timetable.js
+++ web/static/js/selectors/timetable.js
@@ -16,26 +16,60 @@ const timetable = createSelector(
 const crns = createSelector(
   timetable,
   loaded,
-  (timetable, loaded) => (
-    loaded ?
+  (timetable, loaded) => {
+    return loaded ?
       timetable.sections.map(section => section.crn) :
       []
-  )
+  }
 );
 
 const meetingTimes = createSelector(
   timetable,
   loaded,
-  (timetable, loaded) => (
-    loaded ?
-      timetable.sections.map(section => section.meetingTimes) :
-      []
-  )
+  (timetable, loaded) => {
+    if (!loaded) { return []; }
+    let meetingTimes = _.flatten(_.pluck(timetable.sections, "meeting_times"));
+
+    meetingTimes = meetingTimes.map(cast);
+
+    return meetingTimes;
+  }
+)
+
+const days = createSelector(
+  () => {
+    return ["M", "T", "W", "R", "F"];
+  }
+)
+
+const start = createSelector(
+  meetingTimes,
+  (meetingTimes) => {
+    if (_.isEmpty(meetingTimes)) {
+      return new Date(0,0,0,8); // 8am
+    }
+    const earliestMt = _.min(meetingTimes, x => x.start_time);
+    return earliestMt.end_time;
+  }
+)
+
+const end = createSelector(
+  meetingTimes,
+  (meetingTimes) => {
+    if (_.isEmpty(meetingTimes)) {
+      return new Date(0,0,0,17); // 5pm
+    }
+    const latestMt = _.max(meetingTimes, x => x.end_time);
+    return latestMt.end_time;
+  }
 )
 
 export default createStructuredSelector({
   crns,
+  loaded,
   meetingTimes,
+  days,
+  start,
+  end,
   // ticks,
-  // days,
 });
