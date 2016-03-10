98889932c3f528486de0b5b36caf15cf4660524d
Dylan Kendal <dylan@stembolt.com>
2016-03-10 20:02:06 +0800

    Center meeting time text

--- web/static/js/components/Timetable/MeetingTime.jsx
+++ web/static/js/components/Timetable/MeetingTime.jsx
@@ -7,17 +7,44 @@ const Conflict = (props) => (
 
 const MeetingTime = ({section, inConflict, ...rest}) => (
   <g>
-    <rect { ...rest } />
+    <svg
+      x={ rest.x }
+      y={ rest.y }
+      width={ rest.width }
+      height={ rest.height }
+    >
+      <rect
+        fill={ rest.fill }
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+      />
+      <g fontSize="14">
+        <g>
+          <text
+            x="50%"
+            y="50%"
+            dy="-2"
+            textAnchor="middle"
+          >
+            { section.course.subject }
+          </text>
+        </g>
+        <g>
+          <text
+            x="50%"
+            y="50%"
+            dy="12"
+            textAnchor="middle"
+          >
+            <tspan>{ section.course.number }</tspan>
+          </text>
+        </g>
+      </g>
+    </svg>
 
     { inConflict ?  <Conflict { ...rest } /> : null }
-
-    <foreignObject
-      requiredExtensions="http://www.w3.org/1999/xhtml"
-      { ...rest }
-    >
-      <p>{ section.course.subject }</p>
-      <p>{ section.course.number }</p>
-    </foreignObject>
   </g>
 );
 
