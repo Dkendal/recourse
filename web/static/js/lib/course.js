42f22cbf362f7e1abe0f7a864a7fea4fa3c485df
Dylan Kendal <dylan@stembolt.com>
2016-03-11 19:58:21 +0800

    split casting function up

---
+++ web/static/js/lib/course.js
@@ -0,0 +1,15 @@
+function toNum(str) {
+  let value = 0;
+
+  for (let idx = 0; idx < str.length; idx++) {
+    value += str.charCodeAt(idx);
+  }
+
+  return value
+}
+
+export function toColor({ subject, number }) {
+  const hue = (toNum(subject) ** 5 + toNum(number)) * 16 % 255;
+
+  return `hsl(${hue},${80}%,${70}%)`;
+}
--- web/static/js/lib/meeting_time.js
+++ web/static/js/lib/meeting_time.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import { toColor } from "./course";
 
 function toTime(string) {
   return moment(string, "HH:mm").toDate();
@@ -8,38 +9,26 @@ function toDate(string) {
   return moment(string, "YYYY-MM-DD").toDate();
 }
 
-function toColor(mt) {
-  function toNum(str) {
-    let value = 0;
-
-    for (let idx = 0; idx < str.length; idx++) {
-      value += str.charCodeAt(idx);
-    }
-
-    return value
-  }
-
-  const course = mt.section.course;
-  const {subject, number} = course
-
-  const hue = (toNum(subject) ** 5 + toNum(number)) * 16 % 255;
-
-  return `hsl(${hue},${80}%,${70}%)`;
-}
-
-export function cast(mt) {
+export function castDates(mt) {
   const start_time = toTime(mt.start_time);
   const end_time = toTime(mt.end_time);
   const date_end = toDate(mt.date_end)
   const date_start = toDate(mt.date_start)
-  const fill = toColor(mt)
 
   return {
     ...mt,
     date_end,
     date_start,
     end_time,
-    fill,
     start_time,
+  }
+}
+
+export function cast(mt) {
+  const fill = toColor(mt.section.course);
+
+  return {
+    ...castDates(mt),
+    fill,
   }
 }
