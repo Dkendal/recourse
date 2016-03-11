47d389fdc53b386caa78c449d65190fe77f90f76
Dylan Kendal <dylan@stembolt.com>
2016-03-11 09:41:51 +0800

    Remove timetable test for now

--- test/timetable_test.exs
+++ test/timetable_test.exs
@@ -3,10 +3,5 @@ defmodule Recourse.TimetableTest do
   alias Recourse.Timetable
 
   describe ".new/1" do
-    it "creates a new struct" do
-      result = Timetable.new([1, 2, 3])
-      assert result.sections == [1,2,3]
-      assert 3 == result.overlaps |> length
-    end
   end
 end
