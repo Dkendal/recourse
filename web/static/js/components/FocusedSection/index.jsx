003f4c950d9cc86092b06e5a8f9e7d57751833a7
Dylan Kendal <dylan@stembolt.com>
2016-03-20 11:23:40 +0800

    Change icon to close FS

--- web/static/js/components/FocusedSection/index.jsx
+++ web/static/js/components/FocusedSection/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Article from "./Article";
-// import "css/components/FocusedSection";
+import "css/components/FocusedSection";
 
 const FocusedSection = props => (
   <section
@@ -17,8 +17,10 @@ const FocusedSection = props => (
       <Footer { ...props }/>
     </div>
 
-    <button onClick={ props.hideFocusedSection }>
-      <i className="icon-cancel"></i>
+    <button
+      className="FocusedSection-close"
+      onClick={ props.hideFocusedSection }>
+      <i className="icon-angle-double-left"></i>
     </button>
   </section>
 );
