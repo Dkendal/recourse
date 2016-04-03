e4507db246e37ccff741f8fd8f52ddd37606d326
Dylan Kendal <dylan@stembolt.com>
2016-04-03 14:44:41 -0700

    minor formatting changes

--- web/static/js/components/CollectionSelect.jsx
+++ web/static/js/components/CollectionSelect.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import  "css/components/CollectionSelect";
 
 const CollectionSelect = ({className="", collection, selected, render, onClick}) => (
-  <div
-    className={className + " CollectionSelect"}
-    >
+  <div className={className + " CollectionSelect"}>
     { collection.map((item, idx) => (
       <div
         key={idx}
         onClick={() => onClick(item)}
-        style={ { display: 'flex' } }
+        style={{ display: 'flex' }}
         className={ [
           selected.includes(item) ? "selected" : "",
           "CollectionSelect-item",
           "Tile-padded",
           "margin-between-h",
         ].join(" ") }
-        >
+      >
         <div className="icon-gutter">
-          {
-            selected.includes(item) ? (
-              <i className="icon icon-check"></i>
-              ) : (
-              <i className="icon icon-check-empty"></i>
-              )
+          { (selected.includes(item)
+            ? <i className="icon icon-check"></i>
+            : <i className="icon icon-check-empty"></i>)
           }
         </div>
         { render(item) }
