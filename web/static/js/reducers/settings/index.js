7d0ee7b8a9eee2ea64b2e03010d36ca792b29664
Dylan Kendal <dylan@stembolt.com>
2016-04-03 19:15:34 -0700

    move search node into its own file

--- web/static/js/reducers/settings/index.js
+++ web/static/js/reducers/settings/index.js
@@ -3,16 +3,7 @@ import { handleActions } from "redux-actions";
 import time from "lib/time";
 import timetable from "./timetable";
 import courses from "./courses";
-
-const text = handleActions({
-  SET_SETTINGS_SEARCH_TEXT: (state, { payload }) => {
-    return payload;
-  },
-}, "");
-
-const search = combineReducers({
-  text,
-});
+import search from "./search";
 
 export default combineReducers({
   timetable,
