336f20d544e37e573f408d9e8f250b76afeb0af5
Dylan Kendal <dylan@stembolt.com>
2016-04-03 19:14:24 -0700

    Move course settings into its own file.

--- web/static/js/reducers/settings/index.js
+++ web/static/js/reducers/settings/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "redux";
 import { handleActions } from "redux-actions";
-import { LOAD } from "redux-storage";
-import { Set } from "immutable";
 import time from "lib/time";
 import timetable from "./timetable";
+import courses from "./courses";
 
 const text = handleActions({
   SET_SETTINGS_SEARCH_TEXT: (state, { payload }) => {
@@ -13,24 +12,6 @@ const text = handleActions({
 
 const search = combineReducers({
   text,
-});
-
-const selected = handleActions({
-  TOGGLE_SETTINGS_COURSES_SELECTED: (state, { payload }) => {
-    return state.has(payload) ? state.delete(payload) : state.add(payload);
-  },
-  [LOAD]: (state, { payload }) => {
-    return payload &&
-    payload.settings &&
-    payload.settings.courses &&
-    payload.settings.courses.selected &&
-    Set(payload.settings.courses.selected) ||
-    Set();
-  }
-}, Set());
-
-const courses = combineReducers({
-  selected,
 });
 
 export default combineReducers({
