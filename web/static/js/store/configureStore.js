f7803c386995b27bfa2a5ba7ba60f3a77e9dad34
Dylan Kendal <dylan@stembolt.com>
2016-04-03 22:18:25 -0700

    Move reducers into ducks directory

--- web/static/js/store/configureStore.js
+++ web/static/js/store/configureStore.js
@@ -4,7 +4,7 @@ import * as storage from "redux-storage";
 import createEngine from "redux-storage-engine-localstorage";
 import filter from "redux-storage-decorator-filter";
 import debounce from "redux-storage-decorator-debounce";
-import reducer, {loader} from '../reducers';
+import reducer from 'ducks';
 import deserializer from "./deserializer";
 
 let engine = createEngine('recourse');
