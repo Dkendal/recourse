3556d342b005a901f57e8bc45fd54e15ea0aa132
Dylan Kendal <dylan@stembolt.com>
2016-04-03 22:28:51 -0700

    Remove unused actions

--- web/static/js/actions/index.js
+++ web/static/js/actions/index.js
@@ -4,27 +4,9 @@ import settingsActions from "./settings";
 import focusedSectionActions from "./focused_sections";
 import pageActions from "./page";
 
-const filterCourses = createAction("FILTER_COURSES");
-
-function setScheduleSettings({target: {value, name}}) {
-  return (dispatch) => {
-    return dispatch(createAction(`SET_${name}`)(value));
-  };
-}
-
-function changeTerm(term) {
-  return (dispatch) => {
-    dispatch(createAction("CHANGE_TERM")(term));
-    dispatch(channelActions.refreshSchedule());
-  };
-}
-
 export default {
   ...channelActions,
   ...focusedSectionActions,
   ...settingsActions,
   ...pageActions,
-  changeTerm,
-  filterCourses,
-  setScheduleSettings,
 };
