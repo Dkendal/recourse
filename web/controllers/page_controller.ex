79a7d235a6c22a1bb83bb91099fed8f58be4210e
Dylan Kendal <dylan@stembolt.com>
2016-04-09 16:26:08 -0700

    Add empty page for /about

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule Recourse.PageController do
   use Recourse.Web, :controller
 
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
