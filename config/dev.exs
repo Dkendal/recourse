3f242b65a43ed072b595099487a02e008a85b940
Dylan Kendal <dylan@stembolt.com>
2016-08-18 13:13:00 -0700

    remove brunch

--- config/dev.exs
+++ config/dev.exs
@@ -6,13 +6,18 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
-config :recourse, Recourse.Endpoint,
+config(
+  :recourse,
+  Recourse.Endpoint,
   http: [port: {:system, "PORT"}],
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
-  check_origin: false,]]
-  #watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
+  check_origin: false,
+  watchers: [
+    node: ["node_modules/typescript/bin/tsc", "--watch"],
+    bundle: ["exec", "sass", "--scss", "web/static/css/application.scss:priv/static/css/app.css"],
+  ])
 
 # Watch static and templates for browser reloading.
 config :recourse, Recourse.Endpoint,
