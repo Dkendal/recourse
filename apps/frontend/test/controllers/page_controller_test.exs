7e86b33e33e4eabe7ca7aab52972b6127a1b766b
Dylan Kendal <dylan@stembolt.com>
2016-10-02 14:11:54 -0700

    assign terms in controller

--- apps/frontend/test/controllers/page_controller_test.exs
+++ apps/frontend/test/controllers/page_controller_test.exs
@@ -1,8 +1,43 @@
 defmodule Frontend.PageControllerTest do
+  use Frontend.Web, :aliases
   use Frontend.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe"GET index" do
+    setup [:seed, :index]
+
+    test "responsed with 200", %{conn: conn} do
+      assert html_response(conn, 200) =~ "body"
+    end
+
+    test "assigns terms", %{conn: conn} do
+      assert [%Term{year: 2020, semester: :fall}] = conn.assigns[:terms]
+    end
+  end
+
+  def index c do
+    conn = get c.conn, "/"
+    %{c | conn: conn}
+  end
+
+  def seed c do
+    {:ok, _} = Repo.insert %Term{
+      year: 2020,
+      semester: :fall,
+      courses: [
+        %Course{
+          subject: "CSC",
+          number: "100",
+          sections: [
+            %Section{
+              name: "A01",
+              meeting_times: [
+                %MeetingTime{
+                  days: ["M"],
+                  date_end: ~D[2021-04-25],
+                  date_start: ~D[2020-09-05],
+                  start_time: ~T[12:00:00],
+                  end_time: ~T[14:00:00]
+                }]}]}]}
+      c
   end
 end
--- apps/frontend/web/controllers/page_controller.ex
+++ apps/frontend/web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule Frontend.PageController do
+  use Frontend.Web, :aliases
   use Frontend.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    terms = Repo.all Term
+    render conn, "index.html", terms: terms
   end
 end
