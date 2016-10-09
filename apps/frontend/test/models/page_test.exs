f14ca8ae039c4559690ee60deb5605f2b1c160c3
Dylan Kendal <dylan@stembolt.com>
2016-10-09 12:51:31 -0700

    add missing test block

--- apps/frontend/test/models/page_test.exs
+++ apps/frontend/test/models/page_test.exs
@@ -3,7 +3,9 @@ defmodule Frontend.PageTest do
   use Frontend.ModelCase
 
   describe "#changeset/2" do
-    changeset = Page.changeset(%Page{}, %{})
-    assert changeset.valid?
+    test "creating a valid changeset" do
+      changeset = Page.changeset(%Page{}, %{})
+      assert changeset.valid?
+    end
   end
 end
