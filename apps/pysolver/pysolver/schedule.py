877c73ea7721c02a7094fbd19e110527450a6c3e
Dylan Kendal <dylan@stembolt.com>
2016-11-05 12:11:19 -0700

    Add repr for objects

--- apps/pysolver/pysolver/schedule.py
+++ apps/pysolver/pysolver/schedule.py
@@ -9,6 +9,19 @@ class MeetingTime:
         self.end_time = end_time
         self.start_time = start_time
 
+    def __repr__(self):
+        return "<MeetingTime " \
+                "date_end={} " \
+                "date_start={} " \
+                "days={} " \
+                "end_time={} " \
+                "start_time={}>".format(
+                        self.date_end,
+                        self.date_start,
+                        self.days,
+                        self.end_time,
+                        self.start_time)
+
 
 class Section:
     def __init__(self, name, schedule_type, course_id, id, meeting_times):
@@ -20,16 +33,37 @@ class Section:
                               meeting_time in meeting_times]
 
 
+    def __repr__(self):
+        return "<Section " \
+                "name={} " \
+                "schedule_type={} " \
+                "course_id={} " \
+                "id={} " \
+                "meeting_times={}>".format(
+                        self.name,
+                        self.schedule_type,
+                        self.course_id,
+                        self.id, self.meeting_times)
+
 class Group:
     def __init__(self, course_id, schedule_type, sections):
         self.course_id = course_id
         self.schedule_type = schedule_type
         self.sections = [Section(**section) for section in sections]
 
+    def __repr__(self):
+        return "<Group " \
+                "course_id={} " \
+                "schedule_type={} " \
+                "sections={}>".format(
+                        self.course_id,
+                        self.schedule_type,
+                        self.sections)
 
 class Schedule:
     def solve(sections):
         groups = Schedule.group_by_schedule_type(sections)
+        import pdb; pdb.set_trace()
         return groups
 
     def key(section):
