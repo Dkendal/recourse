eb978e7520ddebb9790698003a96470d3c381dd4
Dylan Kendal <dylan@stembolt.com>
2016-11-04 19:48:51 -0700

    create python objects from dicts

--- apps/pysolver/pysolver/schedule.py
+++ apps/pysolver/pysolver/schedule.py
@@ -1,11 +1,36 @@
 import itertools
-from .group import Group
+
+
+class MeetingTime:
+    def __init__(self, date_start, date_end, start_time, days, end_time):
+        self.date_end = date_end
+        self.date_start = date_start
+        self.days = days
+        self.end_time = end_time
+        self.start_time = start_time
+
+
+class Section:
+    def __init__(self, name, schedule_type, course_id, id, meeting_times):
+        self.name = name
+        self.schedule_type = schedule_type
+        self.course_id = course_id
+        self.id = id
+        self.meeting_times = [MeetingTime(**meeting_time) for
+                              meeting_time in meeting_times]
+
+
+class Group:
+    def __init__(self, course_id, schedule_type, sections):
+        self.course_id = course_id
+        self.schedule_type = schedule_type
+        self.sections = [Section(**section) for section in sections]
 
 
 class Schedule:
-
-    def build_group(course_id, schedule_type, sections):
-        return Group(course_id, schedule_type, sections)
+    def solve(sections):
+        groups = Schedule.group_by_schedule_type(sections)
+        return groups
 
     def key(section):
         return (section['course_id'],
@@ -13,6 +38,10 @@ class Schedule:
 
     def group_by_schedule_type(sections):
         sections = sorted(sections, key=Schedule.key)
-        sections = itertools.groupby(sections, key=Schedule.key)
-        result = [Schedule.build_group(*k, v) for k, v in sections]
+        grouped_sections = itertools.groupby(sections, key=Schedule.key)
+        result = list()
+
+        for (course_id, schedule_type), sections in grouped_sections:
+            result.append(Group(course_id, schedule_type, list(sections)))
+
         return result
--- apps/pysolver/test/pysolver/test_schedule.py
+++ apps/pysolver/test/pysolver/test_schedule.py
@@ -104,8 +104,42 @@ def test_group_by_schedule_type(csc_100):
 
     assert lab.schedule_type == 'Lab'
     assert lab.course_id == 28592
-    assert lab.name() == '28592_Lab'
+    sections = list(lab.sections)
+    assert len(sections) == 8
 
     assert lecture.schedule_type == 'Lecture'
     assert lecture.course_id == 28592
-    assert lecture.name() == '28592_Lecture'
+    sections = list(lecture.sections)
+    assert len(sections) == 2
+
+    section, *_ = sections
+    assert section.id == 6552
+    assert section.name == 'A02'
+    assert section.schedule_type == 'Lecture'
+    assert section.course_id == 28592
+
+    assert len(section.meeting_times) == 1
+    meeting_time, = section.meeting_times
+    assert meeting_time.date_end == 736423
+    assert meeting_time.date_start == 736332
+    assert meeting_time.start_time == 36000
+    assert meeting_time.end_time == 40800
+    assert meeting_time.days == ['M', 'R']
+
+def test_solve(csc_100):
+    lecture_sections = [6551, 6552]
+    lab_sections = [6553, 6554, 6555, 6556, 6557, 6558, 6559, 6560]
+
+    result = Schedule.solve(csc_100)
+
+    assert len(result) == 2
+
+    lab, lecture = result
+
+    assert lab.course_id == 28592
+    assert lab.schedule_type == 'Lab'
+    assert lab.section_id in lab_sections
+
+    assert lecture.course_id == 28592
+    assert lecture.schedule_type == 'Lecture'
+    assert lecture.section_id in lecture_sections
